f21a3bb2780138db9c934522d6626012be914856
Enzo Innocenzi <enzo@innocenzi.dev>
2020-08-09 00:45:27 +0200

    chore: bump pest to 0.2.4

--- src/preset.js
+++ src/preset.js
@@ -13,7 +13,7 @@ module.exports = Preset.make('Laravel Pest')
 			'require-dev': {
 				'nunomaduro/collision': '^5.0',
 				'phpunit/phpunit': '^9.0',
-				'pestphp/pest': '^0.2',
+				'pestphp/pest': '^0.2.4',
 				'pestphp/pest-plugin-laravel': '^0.2',
 			},
 			scripts: {
