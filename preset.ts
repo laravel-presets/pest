a85f6c50fb6cea62bd0d03340cff20ac8acfdd92
Enzo Innocenzi <enzo@innocenzi.dev>
2020-11-20 19:21:39 +0100

    refactor: update for preset 0.2

---
+++ preset.ts
@@ -0,0 +1,34 @@
+import { color, Preset } from 'use-preset';
+
+Preset.setName('Pest for Laravel');
+
+// Makes sure composer settings are right.
+Preset.editJson('composer.json')
+	.merge({
+		'minimum-stability': 'dev',
+		'prefer-stable': true,
+	})
+	.withoutTitle();
+
+// Installs the dependencies.
+Preset.execute('composer')
+	.withArguments([
+		'require',
+		'pestphp/pest',
+		'pestphp/pest-plugin-laravel',
+		'--dev',
+		'--no-interaction',
+	])
+	.withTitle(`Installing ${color.magenta('composer')} dependencies...`);
+
+// Runs the Pest artisan command.
+Preset.execute('php')
+	.withArguments(['artisan', 'pest:install', '--no-interaction'])
+	.withTitle(`Setting up ${color.magenta('Pest')}...`);
+
+// Displays instructions.
+Preset.instruct([
+	`Write your own helpers in ${color.magenta('tests/Helpers.php')}`,
+	`Make new tests with ${color.magenta('php artisan pest:test')}`,
+	`Run ${color.magenta('php artisan test')} to start your test suite`,
+]).withHeading("What's next?");
